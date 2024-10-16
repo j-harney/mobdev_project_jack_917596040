c76de53e7152990997eb4c056979666f66907861
jackpharney <jh00185@my.westga.edu>
2024-10-16 11:48:31 -0400

    Implement Emergency Screen Layout

--- lib/ScreensFromHomeScreen/EmergencyScreen/emergency_widget.dart
+++ lib/ScreensFromHomeScreen/EmergencyScreen/emergency_widget.dart
@@ -12,6 +12,51 @@ class EmergencyScreen extends StatelessWidget {
 
     return Scaffold(
         drawer: drawerCreator.createDrawer(context, 'Emergency'),
-        appBar: appBarCreator.createAppBar(context, 'Emergency'));
+        appBar: appBarCreator.createAppBar(context, 'Emergency'),
+        body: Expanded(
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                          child: Container(
+                              margin: EdgeInsets.all(5),
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 1, color: Colors.black),
+                                      backgroundColor:
+                                          const Color.fromARGB(255, 255, 0, 0),
+                                      shape: const BeveledRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.all(Radius.zero))),
+                                  child: const Text('Family Emergency',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255)))))),
+                      Expanded(
+                          child: Container(
+                              margin: EdgeInsets.all(5),
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 1, color: Colors.black),
+                                      backgroundColor: const Color.fromARGB(
+                                          255, 0, 162, 255),
+                                      shape: const BeveledRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.all(Radius.zero))),
+                                  child: const Text('Medical Emergency',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255)))))),
+                    ]))));
   }
 }
